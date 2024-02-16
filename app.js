f3a6b262a6deb1e105459ed673ebb407fecfd50b
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-02-15 19:44:46 -0500

    Used bcrypt to one-way hash password for security safety

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -46,8 +47,10 @@ passport.use(
             if(!user){
                 return done(null, false, {message: "Incorrect username"});
             };
-            if(user.password !== password) {
-                return done(null, false, {message: "Incorrect password"});
+            const match = await bcrypt.compare(password, user.password);
+            if(!match){
+              //passwords do not match!
+              return done(null, false, {message: "Incorrect password"});
             }
             return done(null, user);
         }
@@ -94,21 +97,26 @@ app.get("/", (req, res) => {
     res.render("./views/index")
 });
 app.get("/sign-up", (req, res) => res.render("./views/sign-up-form"));
-app.post("/sign-up", async (req, res, next) => {
 
+app.post("/sign-up", async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    // if err, do something
+    // otherwise, store hashedPassword in DB
     try {
       const user = new User({
         username: req.body.username,
-        password: req.body.password
+        password: hashedPassword
       });
       
       const result = await user.save();
-
-
+      
       res.redirect("/");
     } catch(err) {
       return next(err);
     };
+  });
+
+    
   });
 
   app.post(
